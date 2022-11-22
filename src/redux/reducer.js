fa22028892f4f4ce7f514f6c62556b8769bf7ace
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-11-22 17:40:51 +0200

    combine reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,15 @@
-const initialState = {
+import { combineReducers } from 'redux';
+
+const contactsInitialState = {
   contacts: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ],
-  filter: '',
 };
 
-export const rootReducer = (state = initialState, action) => {
+export const contactsReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
     case 'contacts/addContact': {
       return {
@@ -26,6 +27,17 @@ export const rootReducer = (state = initialState, action) => {
       };
     }
 
+    default:
+      return state;
+  }
+};
+
+const filterInititalState = {
+  filter: '',
+};
+
+export const filterReducer = (state = filterInititalState, action) => {
+  switch (action.type) {
     case 'contacts/setFilter': {
       return {
         ...state,
@@ -37,3 +49,8 @@ export const rootReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
