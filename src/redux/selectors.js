5d7ee7a032264698797a1eb670ec81fd32a51813
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-12-08 14:44:21 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter.filter;
 
-export const selectFilteredContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
-  const filteredContacts = contacts.filter(item => {
-    return item.name.toLowerCase().includes(filter.toLowerCase().trim());
-  });
-
-  return filteredContacts;
-};
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(item => {
+      return item.name.toLowerCase().includes(filter.toLowerCase().trim());
+    });
+  }
+);
