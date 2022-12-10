8bf8aec1e86a0b5f2dd2f61cef91c1a895112088
Pylypeichenko <99480889+Pylypeichenko@users.noreply.github.com>
2022-12-10 15:10:05 +0000

    Deploying to gh-pages from @ Pylypeichenko/goit-react-hw-08-phonebook@21e53051936ae2df5f996d1eb8316bec515275be 🚀

---
+++ static/js/877.10b48096.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[877],{6877:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,a=t(1087),s=t(168),u=t(4934).Z.p(r||(r=(0,s.Z)(["\n  display: flex;\n  justify-content: center;\n"]))),c=t(3329),o=function(){return(0,c.jsxs)(u,{children:["This page was not found. Please return to",(0,c.jsx)(a.rU,{to:"/",children:"main page"})]})}}}]);
+//# sourceMappingURL=877.10b48096.chunk.js.map
